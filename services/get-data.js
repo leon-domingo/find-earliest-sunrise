8b94e608cc57a82c70f99f32c0aa7b50c6dee224
León Domingo <leon.domingo@gmail.com>
2021-06-27 19:44:16 +0200

    * Minor changes

--- services/get-data.js
+++ services/get-data.js
@@ -20,8 +20,25 @@ const getSunriseSunsetDataMock = (failThreshold = .75, delay = 2000) =>
   async function(apiURL, latitude, longitude, date = 'today') {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (Math.random() < failThreshold) {
-          resolve({ apiURL, latitude, longitude, date });
+        if (Math.random() > failThreshold) {
+          resolve({
+            '__MOCKUP__': {
+              apiURL,
+              latitude,
+              longitude,
+              date,
+            },
+            'sunrise': '9:44:20 AM',
+            'sunset': '9:33:08 PM',
+            'solar_noon': '5:19:14 PM',
+            'day_length': '15:09:48',
+            'civil_twilight_begin': '9:10:26 AM',
+            'civil_twilight_end': '1:28:03 AM',
+            'nautical_twilight_begin': '8:26:51 AM',
+            'nautical_twilight_end': '2:11:38 AM',
+            'astronomical_twilight_begin': '7:34:44 AM',
+            'astronomical_twilight_end': '3:03:45 AM',
+          });
         } else {
           const error = new Error('REQUEST FAILED');
           reject(error);
